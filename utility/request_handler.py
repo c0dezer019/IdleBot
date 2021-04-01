eb60714727f37d35c70f236a8bb5a9ae8c021888
c0dezer019 <c0dezer019@zohomail.com>
2021-03-31 22:20:44 -0500

    Functioning routing.

--- utility/request_handler.py
+++ utility/request_handler.py
@@ -6,39 +6,39 @@ api_base_url_prod = 'https://combot.bblankenship.me/v1/'
 
 def add_server(guild):
     api_add_server = '{0}bot/servers/add'.format(api_base_url_dev)
-    api_get_server = '{0}bot/servers/{1}'.format(api_base_url_dev, guild.id)
+    api_get_server = '{0}bot/servers/{1}'.format(api_base_url_dev, guild['server_id'])
     server = requests.get(api_get_server)
 
-    if server.status_code == 500:
-        packet = { 'server_id': guild.id, 'name': guild.name }
-        response = requests.post(api_add_server, packet)
+    if server.status_code == 404:
+        packet = { 'server_id': guild['server_id'], 'name': guild['name'] }
+        response = requests.post(api_add_server, json = packet)
 
         return response
     else:
         return 200
 
 
-def add_users(db_users, server_users, guild_id):
+def add_users(server_users, db_users, guild_id):
     api_add_user = '{0}bot/users/add'.format(api_base_url_dev)
     db_user_ids = []
     errors = []
-    response = {}
+    response = { }
 
     for i, v in enumerate(db_users):
-        db_user_ids.insert(len(db_user_ids), v.user_id)
+        db_user_ids.insert(len(db_user_ids), v.id)
 
     for i, v in enumerate(server_users):
         if v.id not in db_user_ids:
-            packet = [{ 'user_id': v.id, 'username': v }, { 'server_id': guild_id }]
-            res = requests.post(api_add_user, packet)
+            packet = { 'user_id': v.id, 'username': f'{v.name}#{v.discriminator}', 'server_id': guild_id }
+            res = requests.post(api_add_user, json = packet)
 
             if res.status_code == 200:
                 response = res
             else:
-                errors.insert(len(errors), {'res_code': res.status_code, 'user': {'id': v.id, 'username': v}})
+                errors.insert(len(errors), { 'res_code': res.status_code, 'user': { 'id': v.id, 'username': v } })
 
     if len(errors) > 0:
-        return errors, response
+        return response, errors
 
     return response
 
@@ -49,8 +49,6 @@ def handle_users(guild):
 
     if res.status_code == 200:
         users = res.json()
-        response = add_users(guild.users, users, guild.server_id)
+        response = add_users(guild['users'], users, guild['server_id'])
 
         return response
-    else:
-        return res.status_code
