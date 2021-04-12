629332e5916aaeabbc7efad7a6c7d4a320987af4
c0dezer019 <c0dezer019@zohomail.com>
2021-04-12 01:06:25 -0500

    Updated error message.

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ intents = discord.Intents.default()
 intents.members = True
 
 bot = commands.Bot(command_prefix = '?', description = description, intents = intents)
-extensions = ['cogs.admin_cmds', 'cogs.error_handler', 'cogs.listeners', 'cogs.setup_tasks', 'cogs.usr_cmds']
+extensions = ['cogs.admin_cmds', 'cogs.listeners', 'cogs.setup_tasks', 'cogs.usr_cmds']
 
 
 @bot.event
@@ -21,7 +21,7 @@ async def on_error(event, *args, **kwargs):
     message = args[0]
     logging.warning(traceback.format_exc())
 
-    await message.guild.system_channel.send('Oh my god, {0.mention} has caused an error.'.format(message.author))
+    await message.guild.system_channel.send('I have encountered an error but do not worry, I will alert my owner.')
 
 
 if __name__ == '__main__':
