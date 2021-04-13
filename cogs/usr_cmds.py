100ff73676da8bf9b203c28d93f1731554ac05cb
c0dezer019 <c0dezer019@zohomail.com>
2021-04-13 01:15:30 -0500

    Refactoring to work with backend.

--- cogs/usr_cmds.py
+++ cogs/usr_cmds.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
 from utility.helpers import check_idle_time, filter_channels, get_user_last_message, get_messages, generate_idle_msg
+import utility.request_handler as rh
 
 
 class UserCommands(commands.Cog):
@@ -8,28 +9,7 @@ class UserCommands(commands.Cog):
 
     @commands.Cog.listener()
     async def ping(self, ctx, args):
-        members = ctx.message.guild.members
-        user = ''
-
-        if args:
-            for i, v in enumerate(members):
-                if args == v.name:
-                    user = v
-                    break
-
-            if not user:
-                await ctx.channel.send(f'{args} is not a valid user or has changed their name.')
-
-        text_channels = filter_channels(ctx.message.guild.channels)
-        all_messages = await get_messages(text_channels)
-        user_last_message = get_user_last_message(all_messages, user)
-        time_idle = check_idle_time(user_last_message.created_at.replace(tzinfo = None))
-        response = generate_idle_msg(time_idle)
-
-        # if user has no recent activity, label as inactive.
-        # if user has recent activity, update the appropriate fields in the database.
-
-        await ctx.channel.send(f'{user.name}' + response)
+        pass
 
 
 def setup(bot):
