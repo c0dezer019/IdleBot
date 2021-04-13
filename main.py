8d664c2fdc2131148cd69a50d47c4cdb69703b33
c0dezer019 <c0dezer019@zohomail.com>
2021-04-13 01:15:01 -0500

    Error handling update.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
 from dotenv import load_dotenv
+from logging.handlers import RotatingFileHandler
 import discord
 import logging
 import traceback
@@ -12,6 +13,13 @@ description = '''A bot to enforce the rules.'''
 intents = discord.Intents.default()
 intents.members = True
 
+logger = logging.getLogger('Error Log')
+logger.setLevel(logging.ERROR)
+handler = RotatingFileHandler('error_log.txt', maxBytes = 10000, backupCount = 5)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
 bot = commands.Bot(command_prefix = '?', description = description, intents = intents)
 extensions = ['cogs.admin_cmds', 'cogs.listeners', 'cogs.setup_tasks', 'cogs.usr_cmds']
 
@@ -19,7 +27,8 @@ extensions = ['cogs.admin_cmds', 'cogs.listeners', 'cogs.setup_tasks', 'cogs.usr
 @bot.event
 async def on_error(event, *args, **kwargs):
     message = args[0]
-    traceback.print_exc(file = 'utility/error_log')
+
+    logger.error(f'Error happened within {event}:\n{traceback.format_exc()}',)
 
     await message.guild.system_channel.send('I have encountered an error but do not worry, I will alert my owner.')
 
