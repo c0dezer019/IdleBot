efd83a8a68a224aa957c8b869fb9d69393cd8e90
c0dezer019 <c0dezer019@zohomail.com>
2021-04-17 09:53:40 -0500

    Rewrote ping command to work with backend.

--- cogs/usr_cmds.py
+++ cogs/usr_cmds.py
@@ -1,15 +1,35 @@
+from datetime import datetime
 from discord.ext import commands
-from utility.helpers import check_idle_time, filter_channels, get_user_last_message, get_messages, generate_idle_msg
+from utility.helpers import check_idle_time
 import utility.request_handler as rh
 
 
 class UserCommands(commands.Cog):
+
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.Cog.listener()
-    async def ping(self, ctx, args):
-        pass
+    @commands.command()
+    async def ping(self, ctx, arg):
+        guild = rh.get_guild(ctx.message.guild.id)
+        iso_timestamp = guild['last_activity_ts']
+        timestamp = datetime.fromisoformat(iso_timestamp)
+        get_idle_time = check_idle_time(timestamp)
+        years = 0
+
+        if get_idle_time['days'] >= 365:
+            years = round(get_idle_time['days'] / 365)
+            get_idle_time['days'] = get_idle_time['days'] % 365
+
+        idle_time_str = f'{get_idle_time["days"]} days, {get_idle_time["hours"]} hours, and ' \
+                        f'{get_idle_time["minutes"]} minutes'
+
+        if years > 0:
+            idle_time_str = f'{years} years, ' + idle_time_str
+
+        response_str = f'Last activity in {ctx.guild} was performed {idle_time_str} ago.'
+
+        await ctx.guild.system_channel.send(response_str)
 
 
 def setup(bot):
