965a59b570fd32490c85247047b02f61870f60b5
c0dezer019 <c0dezer019@zohomail.com>
2021-04-17 09:54:48 -0500

    Various things.

--- utility/request_handler.py
+++ utility/request_handler.py
@@ -1,41 +1,44 @@
 import requests
+import json
 
 api_base_url_dev = 'http://127.0.0.1:5000/'
 api_base_url_prod = 'https://combot.bblankenship.me/v1/'
 
 
-def add_guild(guild):
-    api_get_guild = '{0}bot/guilds/{1}'.format(api_base_url_dev, guild['guild_id'])
-    api_add_guild = '{0}bot/guilds/add'.format(api_base_url_dev)
+def add_guild(guild_info):
+    api_get_guild = f'{api_base_url_dev}bot/guilds/{guild_info["guild_id"]}'
+    api_add_guild = f'{api_base_url_dev}bot/guilds/add'
     guild = requests.get(api_get_guild)
 
-    if guild.status_code == 400:
-        packet = { 'guild_id': guild['guild_id'], 'name': guild['name'] }
-        response = requests.post(api_add_guild, json = packet)
+    if guild.status_code == 404:
+        response = requests.post(api_add_guild, json = guild_info)
 
         return response
     else:
-        return 200
+        return guild
 
 
 def add_members(guild_members, db_members, guild_id):
-    api_add_member = '{0}bot/members/add'.format(api_base_url_dev)
+    api_add_member = f'{api_base_url_dev}bot/members/add'
     db_member_ids = []
     errors = []
     response = {}
 
     for i, v in enumerate(db_members):
-        db_member_ids.insert(len(db_member_ids), v)
+        db_member_ids.insert(len(db_member_ids), v['member_id'])
 
     for i, v in enumerate(guild_members):
         if v.id not in db_member_ids:
-            packet = { 'member_id': v.id, 'membername': f'{v.name}#{v.discriminator}', 'guild_id': guild_id }
-            res = requests.post(api_add_member, json = packet)
+            packet = { 'member_id': v.id, 'username': f'{v.name}#{v.discriminator}', 'guild_id': guild_id }
+            response = requests.post(api_add_member, json = packet)
 
-            if res.status_code == 200:
-                response = res
+            if response.status_code == 200:
+                response = response.status_code
             else:
-                errors.insert(len(errors), { 'res_code': res.status_code, 'member': { 'id': v.id, 'membername': v } })
+                errors.insert(len(errors), {
+                    'res_code': response.status_code,
+                    'member': { 'id': v.id, 'username': v }
+                })
 
     if len(errors) > 0:
         return response, errors
@@ -44,11 +47,21 @@ def add_members(guild_members, db_members, guild_id):
 
 
 def get_guild(guild_id):
-    api_get_guild = '{0}bot/guilds/{1}'.format(api_base_url_dev, guild_id)
+    api_get_guild = f'{api_base_url_dev}bot/guilds/{guild_id}'
+    response = requests.get(api_get_guild)
+
+    return response.json()
+
+
+def get_member(member_id):
+    api_get_member = f'{api_base_url_dev}bot/guilds/{member_id}'
+    response = requests.get(api_get_member)
+
+    return response
 
 
 def handle_members(guild):
-    api_get_members = '{0}bot/members'.format(api_base_url_dev)
+    api_get_members = f'{api_base_url_dev}bot/members'
     res = requests.get(api_get_members)
 
     if res.status_code == 200:
@@ -59,7 +72,7 @@ def handle_members(guild):
 
 
 def update_guild(guild_id, **data):
-    api_get_guild = '{0}bot/guilds/{1}'.format(api_base_url_dev, guild_id)
+    api_get_guild = f'{api_base_url_dev}bot/guilds/{guild_id}'
     data_packet = {}
 
     for k, v in data.items():
@@ -74,7 +87,7 @@ def update_guild(guild_id, **data):
 
 
 def update_member(member_id, **data):
-    api_get_member = '{0}bot/members/{1}'.format(api_base_url_dev, member_id)
+    api_get_member = f'{api_base_url_dev}bot/members/{member_id}'
     data_packet = {}
 
     for k, v in data.items():
