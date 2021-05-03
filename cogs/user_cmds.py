a1cb47557f2ef56f23f0b73a6c5d8adcd272acbd
c0dezer019 <c0dezer019@zohomail.com>
2021-05-02 23:55:32 -0500

    Adding a dict for helper strings.

--- cogs/user_cmds.py
+++ cogs/user_cmds.py
@@ -1,18 +1,32 @@
-from datetime import datetime
-
 import discord
+from datetime import datetime
 from discord.ext import commands
 from discord.utils import find
+from typing import Dict
+from requests import Response
 from utility.helpers import check_idle_time
 import utility.request_handler as rh
 
 
 class UserCommands(commands.Cog):
 
-    def __init__(self, bot):
-        self.bot = bot
+    def __init__(self, bot: discord.Client):
+        self.bot: discord.Client = bot
 
-    @commands.command()
+    help_lib: Dict = {
+        'check_brief': 'Returns the idle time of a specified user, or the guild if no user is mentioned.',
+        'check_help': 'Syntax: ?check @<user: Optional>\n\nThis command can be shorted to ch. In order for it '
+                      'to work correctly, you should mention the user, however, as long as the given name '
+                      'matches a member in the guild, it will work, but as members can have the same nicknames/'
+                      'usernames it may not give the correct results.',
+        'performance_brief': 'Provides performance stats of the bot.',
+        'performance_help': 'Syntax: ?performance\n\nCan be shorted to "per". This command returns various '
+                             'performance stats for the bot such as latency between bot and Discord API and latency '
+                             'the bot\'s front and backend.'
+
+    }
+
+    @commands.command(aliases = ['ch'], brief = help_lib['check_brief'], help = help_lib['check_help'])
     async def check(self, ctx: commands.Context, member: discord.Member):
         command = ctx.message.content.split(' ')[1]
 
@@ -23,36 +37,36 @@ class UserCommands(commands.Cog):
                             'discriminator, the #).')
 
         if member is not None:
-            response = rh.get_member(member.id)
+            response: Response = rh.get_member(member.id)
         else:
-            response = rh.get_guild(ctx.message.guild.id)
+            response: Response = rh.get_guild(ctx.message.guild.id)
 
-        response_as_dict = response.json()['member' if member else 'guild']
-        iso_timestamp = response_as_dict['last_activity_ts']
-        status = response_as_dict['status']
-        timestamp = datetime.fromisoformat(iso_timestamp)
-        get_idle_time = check_idle_time(timestamp)
+        response_as_dict: Dict = response.json()['member' if member else 'guild']
+        iso_timestamp: str = response_as_dict['last_activity_ts']
+        status: str = response_as_dict['status']
+        timestamp: datetime = datetime.fromisoformat(iso_timestamp)
+        get_idle_time: Dict = check_idle_time(timestamp)
 
         if status != 'active':
             await ctx.reply(f'I\'m sorry, but {response_as_dict["name"] if member is None else response_as_dict["username"]} is not currently active.')
         else:
-            idle_time_str = f'{get_idle_time["days"]} days, {get_idle_time["hours"]} hours, and ' \
-                            f'{get_idle_time["minutes"]} ' \
-                            f'{"minutes" if get_idle_time["minutes"] > 1 or get_idle_time["minutes"] == 0 else "minute"}'
+            idle_time: str = f'{get_idle_time["days"]} days, {get_idle_time["hours"]} hours, and ' \
+                                 f'{get_idle_time["minutes"]} ' \
+                                 f'{"minutes" if get_idle_time["minutes"] > 1 or get_idle_time["minutes"] == 0 else "minute"}'
 
             if get_idle_time['days'] >= 365:
                 years = round(get_idle_time['days'] / 365)
                 get_idle_time['days'] = get_idle_time['days'] % 365
-                idle_time_str = f'{get_idle_time["days"]} days, {get_idle_time["hours"]} hours, and ' \
-                                f'{get_idle_time["minutes"]} ' \
-                                f'{"minutes" if get_idle_time["minutes"] > 1 or get_idle_time["minutes"] == 0 else "minute"}'
-                idle_time_str = f'{years} years, ' + idle_time_str
+                idle_time: str = f'{get_idle_time["days"]} days, {get_idle_time["hours"]} hours, and ' \
+                                 f'{get_idle_time["minutes"]} ' \
+                                 f'{"minutes" if get_idle_time["minutes"] > 1 or get_idle_time["minutes"] == 0 else "minute"}'
+                idle_time: str = f'{years} years, ' + idle_time
 
-            response_str = f'Last activity for {response_as_dict["name"] if member is None else response_as_dict["username"]} was ' \
-                           f'performed {idle_time_str} ago.'
+            response_str: str = f'Last activity for {response_as_dict["name"] if member is None else response_as_dict["username"]} was ' \
+                                f'performed {idle_time} ago.'
 
-            general = find(lambda x: x.name == 'general', ctx.guild.text_channels)
-            sys_chan = ctx.guild.system_channel
+            general: discord.TextChannel = find(lambda x: x.name == 'general', ctx.guild.text_channels)
+            sys_chan: discord.TextChannel = ctx.guild.system_channel
 
             if sys_chan and sys_chan.permissions_for(ctx.guild.me).send_messages:
                 await sys_chan.send(response_str)
@@ -63,6 +77,10 @@ class UserCommands(commands.Cog):
     async def check_error_handler(self, ctx: commands.Context, error):
         if isinstance(error, commands.MemberNotFound):
             await ctx.reply('That member was not found.')
+
+    @commands.command(aliases = ['per'], brief = help_lib['performance_brief'], help = help_lib['performance_help'])
+    async def performance(self, ctx: commands.Context):
+        pass
 
 
 def setup(bot):
