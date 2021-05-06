ac6742ccf09e1ce96633345a578ce9870fd54cc5
c0dezer019 <c0dezer019@zohomail.com>
2021-05-06 00:21:20 -0500

    stuff.

--- utility/helpers.py
+++ utility/helpers.py
@@ -1,31 +1,28 @@
-from datetime import datetime, timedelta
-from logging.handlers import RotatingFileHandler
-from pytz import timezone
+from datetime import timedelta
+import arrow
+import datetime
 import logging
 
+logger = logging.getLogger('IdleBot Logger')
 
-def log(level: int, log_file: str):
-    logger = logging.getLogger('Bot Log')
-    logger.setLevel(level)
-    handler = RotatingFileHandler(f'../{log_file}', maxBytes = 500000, backupCount = 5)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
 
-    return logger
+def calculate_average_idle_time():
+    pass
+
 
 # measures the amount of idle time between now and then (the given timestamp, which should be user's last message or
 # last active in voice comms.
 
+def check_idle_time(ts: datetime.datetime):
 
-def check_idle_time(ts: datetime):
-    central_tz = timezone('US/Central')
-    current_time = datetime.now(central_tz)
+    current_time = arrow.now('US/Central').datetime
     difference = current_time - ts
     duration_in_seconds = difference.total_seconds()
     days = timedelta(seconds = duration_in_seconds).days
     remaining_seconds = timedelta(seconds = duration_in_seconds).seconds
     hours = remaining_seconds // 3600
     minutes = (remaining_seconds // 60) % 60
+    time_idle = { 'days': days, 'hours': hours, 'minutes': minutes }
 
-    return { 'days': days, 'hours': hours, 'minutes': minutes }
+    return time_idle
+
